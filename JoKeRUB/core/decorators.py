5d5e80fb4f7b7d0d5ef2b8cc51cd3fc061a34f18
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2023-12-25 22:03:13 +0200

    Update decorators.py

--- JoKeRUB/core/decorators.py
+++ JoKeRUB/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "هذا الخيار ليس لك يجب عليك تنصيب سورس الجوكر بنفسك  @jepthon"
+                or "هذا الخيار ليس لك يجب عليك تنصيب سورس عفرتو بنفسك  @T_Y_E_X"
             )
             await c_q.answer(
                 HELP_TEXT,
