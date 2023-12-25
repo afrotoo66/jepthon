d27e7a423ebb9f2b87f1e0270e5ac7d137fc5736
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2023-12-25 21:05:58 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -25,10 +25,10 @@ class Config(object):
     )
     TG_BOT_USERNAME = None
     # get this value from http://www.timezoneconverter.com/cgi-bin/findzone.tzc
-    TZ = os.environ.get("TZ", "Asia/Baghdad")
+    TZ = os.environ.get("TZ", "Africa/Cairo")
     # set this with required cat repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/jepthoniq/jepthon.git"
+        "UPSTREAM_REPO", "https://github.com/afrotoo66/jepthon.git"
     )
 
     # BASIC and MAIN CONFIG VARS
